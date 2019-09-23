43a40a90918ec1d219e727ac53aaff3b9a365317
7117 <qinglangsalaheiyou@163.com>
2019-09-23 16:53:18 +0800

    PHP的roud函数

---
+++ PHP/PHP数学函数/1.round.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunxi
+ * Date: 2019/9/23
+ * Time: 16:50
+ */
+
+//float(111.22) float(111.48)
+$a=round(111.222,2);
+var_dump($a);
+$a=round(111.476,2);
+var_dump($a);
+
+//11
+$a=round(11.11);
+var_dump($a);
