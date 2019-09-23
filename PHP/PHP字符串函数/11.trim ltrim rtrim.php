cb6b2b4411ea3d8ebc31f5cdac426f88e740b1ba
7117 <qinglangsalaheiyou@163.com>
2019-09-23 19:00:02 +0800

    trim ltrim rtrim

---
+++ PHP/PHP字符串函数/11.trim ltrim rtrim.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunxi
+ * Date: 2019/9/23
+ * Time: 18:58
+ */
+
+$a='  aaa   ';
+$b=ltrim($a);
+var_dump($b);
+echo "<br>";
+
+$a='  aaa   ';
+$b=rtrim($a);
+var_dump($b);
+echo "<br>";
+
+
+$a='  aaa   ';
+$b=trim($a);
+var_dump($b);
+echo "<br>";
