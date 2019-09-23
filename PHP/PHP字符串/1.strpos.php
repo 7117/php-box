712050ecc2e54e2cb302444aaefddb8f38cc90e4
7117 <qinglangsalaheiyou@163.com>
2019-09-23 16:34:15 +0800

    str_replace与strpos

---
+++ PHP/PHP字符串/1.strpos.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunxi
+ * Date: 2019/9/23
+ * Time: 16:27
+ */
+
+//strpos
+$num=strpos('aaav','v');
+//输出3
+var_dump($num);
+
+
---
+++ PHP/PHP字符串/2.str_replace.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunxi
+ * Date: 2019/9/23
+ * Time: 16:30
+ */
+
+$aa=str_replace('a','v','gva',$count1);
+var_dump($aa);
+echo "<br>";
+var_dump($count1);
+
+echo "<br>";
+$aa=str_replace('a','v','gvaaaaaavaaa',$count2);
+var_dump($aa);
+echo "<br>";
+var_dump($count2);
