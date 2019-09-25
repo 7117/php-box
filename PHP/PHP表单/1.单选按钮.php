6866d2dace9e3e99688ef9c9ead750c1d8afd810
7117 <qinglangsalaheiyou@163.com>
2019-09-25 23:03:42 +0800

    单选按钮

---
+++ PHP/PHP表单/1.单选按钮.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>	
+	<form name="aaa" action="1.单选按钮.php">
+		<input type="radio" name="aaaa">
+		<input type="radio" name="aaaa">
+		<input type="radio" name="aaaa">
+		<input type="radio" name="aaaa">
+	</form>
+
+</body>
+</html>
