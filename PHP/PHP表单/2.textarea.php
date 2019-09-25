c12f929d8a87ed643a8c56218c00565c681efb8f
7117 <qinglangsalaheiyou@163.com>
2019-09-25 23:31:36 +0800

    textarea

---
+++ PHP/PHP表单/2.textarea.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>	
+	<form name="aaa" action="1.单选按钮.php">
+		<input type="textarea" style="width:200px;height:200px" name="aaaa">
+	</form>
+
+</body>
+</html>
+
+
+
