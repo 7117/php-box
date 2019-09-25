29e1cc60c1da78a7ed4b892c5d80c89a3224d7db
7117 <qinglangsalaheiyou@163.com>
2019-09-25 23:40:47 +0800

    date()

---
+++ PHP/PHP时间函数/2.date.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$a=date("YMD");
+var_dump($a);
+echo "<br>";
+$a=date("Y");
+var_dump($a);
+echo "<br>";
+$c=date("Y-M-D");
+var_dump($c);
+
+$d=date("Y m d h:i:s");
+echo "<br>";
+var_dump($d);
