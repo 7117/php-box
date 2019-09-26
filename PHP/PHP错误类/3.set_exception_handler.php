082a15b37c60d13e38af8c33699a3d8fb470d1dd
7117 <qinglangsalaheiyou@163.com>
2019-09-27 00:54:46 +0800

    错误累set_exception_handler("aaa")

---
+++ PHP/PHP错误类/3.set_exception_handler.php
@@ -0,0 +1,16 @@
+<?php
+//try{
+//    if(3>2){
+//        throw new Exception("aaa");
+//    }
+//}catch(Exception $e){
+//    echo $e->getMessage();
+//}
+//echo "<br>";
+
+function aaa(Exception $e){
+    echo $e->getMessage();
+}
+
+set_exception_handler("aaa");
+throw new Exception("bbbb");
