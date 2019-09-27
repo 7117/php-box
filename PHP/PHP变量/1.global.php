52d12eb1959b26043a7fb0a3fc01ef9ef3cd053e
7117 <qinglangsalaheiyou@163.com>
2019-09-27 18:39:29 +0800

    $GLOBAL['name']

--- PHP/PHP变量/1.global.php
+++ PHP/PHP变量/1.global.php
@@ -15,16 +15,3 @@ echo $y; // 输出 15
 echo "<br>";
 
 
-// $GLOBAL['']
-$x=511;
-$y=110;
- 
-function myTsssssest()
-{
-	global $y;
-    $y=$GLOBALS['x']+$GLOBALS['y'];
-}  
-myTsssssest();
-echo $y;
-echo "<br>";
-// 621
---
+++ PHP/PHP变量/2.$GLOBAL['名字'].php
@@ -0,0 +1,16 @@
+<?php
+
+//621这个还不错！！！！
+// $GLOBAL['']
+$x=511;
+$y=110;
+
+function myTsssssest()
+{
+    global $yuu;
+    $yuu=$GLOBALS['x']+$GLOBALS['y'];
+}
+myTsssssest();
+echo $yuu;
+echo "<br>";
+// 621
