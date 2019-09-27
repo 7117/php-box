34b730642ffd18f906e6fbba1ff52e463648dc95
7117 <qinglangsalaheiyou@163.com>
2019-09-27 12:08:34 +0800

    记住哦：静态变量用unset销毁不了

--- PHP/PHP变量/1.static.php
+++ PHP/PHP变量/1.static.php
@@ -25,12 +25,16 @@ function myTsssssest()
 myTsssssest();
 echo $y;
 echo "<br>";
-
+// 621
 
 
 // static
 function myTest()
 {
+	// 通常，当函数完成/执行后，会删除所有变量。
+	// 不过，有时我需要不删除某个局部变量。实现这一点需要更进一步的工作。
+	// 一般的话  函数执行完成之后会进行删除变量
+	// 但是static标注的变量不会被删除  会进行保留
     static $x=0;
     echo $x;
     $x++;
@@ -47,3 +51,73 @@ function myTesst($x)
     echo $x;
 }
 myTesst(5);
+
+// 0 1 2 5
+
+
+// static
+// 记住哦  静态变量用unset进行销毁不了
+function wdwdw()
+{
+
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+    static $x=0;
+    echo $x;
+    $x++;
+    unset($x);
+    echo "<br>";
+}
+ 
+wdwdw();
+wdwdw();
+wdwdw();
+
+
+// static
+// 记住哦  静态变量用unset进行销毁不了
+function dwdwdww()
+{
+
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+    $x=0;
+    echo $x;
+    $x++;
+    // unset($x);
+    echo "<br>";
+}
+ 
+dwdwdww();
+dwdwdww();
+dwdwdww();
+
+
+// 15
+// 621
+// 0 1 2 5
+
+
+// 0
+
+
+
+// 1
+
+
+
+// 2
+
+
+
+// 0
+
+
+
+// 0
+
+
+
+// 0
---
+++ PHP/PHP变量/2.kuohao.php
@@ -0,0 +1,5 @@
+<?php
+
+$a="aaaa";
+var_dump("{$a}");
+echo "<br>";
