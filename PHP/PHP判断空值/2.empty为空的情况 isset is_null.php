bd0126afa12553e38045147e1c95a4f3980781ac
7117 <qinglangsalaheiyou@163.com>
2019-09-28 22:40:07 +0800

    empty判断为空的情况

--- PHP/PHP判断空值/2.empty为空的情况 isset is_null.php
+++ PHP/PHP判断空值/2.empty为空的情况 isset is_null.php
@@ -1,10 +1,58 @@
-<html>
-<body>
-<form action="3.wel.php" method="get">
-    Name: <input type="text" name="name" />
-    Age: <input type="text" name="age" />
-    <input type="submit" />
-</form>
+<?php
+// isset空
+// is_null空
+// 空空空空空空空
+$a=null;
+if(!isset($a)){
+    echo "isset空";
+    echo "<br>";
+}
 
-</body>
-</html>
+$a=null;
+if(is_null($a)){
+    echo "is_null空";
+    echo "<br>";
+}
+
+
+$a=0;
+if(empty($a)){
+    echo "空";
+}
+
+$a=[];
+if(empty($a)){
+    echo "空";
+}
+
+$a="";
+if(empty($a)){
+    echo "空";
+}
+
+$a=false;
+if(empty($a)){
+    echo "空";
+}
+
+$a=null;
+if(empty($a)){
+    echo "空";
+}
+
+$a="0";
+if(empty($a)){
+    echo "空";
+}
+
+
+$a=111;
+unset($a);
+if(empty($a)){
+    echo "空";
+}
+
+
+
+
+
