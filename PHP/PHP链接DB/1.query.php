b809e4edf8305e32f98af0aef862d8f45f9152f6
7117 <qinglangsalaheiyou@163.com>
2019-09-29 00:33:43 +0800

    mysqli——fetch_all mysqli_num  mysqli_assoc

--- PHP/PHP链接DB/1.query.php
+++ PHP/PHP链接DB/1.query.php
@@ -3,12 +3,24 @@ include "conn.php";
 
 $sql="select * from user";
 $res=$conn->query($sql);
-$res=mysqli_fetch_all($res,MYSQLI_NUM);
+$res1=mysqli_fetch_all($res,MYSQLI_NUM);
+$res=$conn->query($sql);
+$res2=mysqli_fetch_all($res,MYSQLI_ASSOC);
 
-foreach($res as $k=>$v){
-    var_dump($v);
+echo "<br>";
+var_dump($res1);
+foreach($res1 as $k=>$v){
+    // var_dump($v);
     echo "<br>";
 }
+
+echo "<br>";
+var_dump($res2);
+foreach($res2 as $k=>$v){
+    // var_dump($v);
+    echo "<br>";
+}
+
 
 mysqli_close($conn);
 
