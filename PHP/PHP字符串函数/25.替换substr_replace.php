3b1194ec2a20182d78b7faf4a7370f9a5623e13e
7117 <qinglangsalaheiyou@163.com>
2019-09-29 11:50:08 +0800

    四种替换！

---
+++ PHP/PHP字符串函数/25.替换substr_replace.php
@@ -0,0 +1,9 @@
+<?php
+// ad
+$a=substr_replace("aaaaaffffdddd", "d", 1);
+print_r($a);
+echo "<br>";
+
+// daaaaffffdddd
+$a=substr_replace("aaaaaffffdddd", "d", 0,1);
+print_r($a);
---
+++ PHP/PHP字符串函数/26.替换str_replace.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunxi
+ * Date: 2019/9/23
+ * Time: 16:30
+ */
+
+$aa=str_replace('a','v','gva',$count1);
+var_dump($aa);
+echo "<br>";
+var_dump($count1);
+
+echo "<br>";
+$aa=str_replace('a','v','gvaaaaaavaaa',$count2);
+var_dump($aa);
+echo "<br>";
+var_dump($count2);
+
+// string(3) "gvv"
+// int(1)
+// string(12) "gvvvvvvvvvvv"
+// int(9)
+
