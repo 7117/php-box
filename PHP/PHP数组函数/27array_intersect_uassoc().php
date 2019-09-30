e8f65f920c4b2faaabc5c08c135cf662dbf52511
7117 <qinglangsalaheiyou@163.com>
2019-09-30 16:37:35 +0800

    array_key_exists

--- PHP/PHP数组函数/27array_intersect_uassoc().php
+++ PHP/PHP数组函数/27array_intersect_uassoc().php
@@ -3,4 +3,3 @@
 $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
 $a2=array("a"=>"red","b"=>"greedddn","dc"=>"blue","d"=>"yellow");
 
-
--- PHP/PHP数组函数/28array_key_exists.php
+++ PHP/PHP数组函数/28array_key_exists.php
@@ -1,5 +1,7 @@
 <?php
 
 $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-$a2=array("a"=>"red","b"=>"greedddn","dc"=>"blue","d"=>"yellow");
 
+$aaa=array_key_exists("a",$a1);
+var_dump($aaa);
+// true
