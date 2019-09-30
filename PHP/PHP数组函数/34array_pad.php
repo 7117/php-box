676cc9cdd177273e64e6744aa137f7d91032bc75
7117 <qinglangsalaheiyou@163.com>
2019-09-30 17:07:16 +0800

    arrar_pad填充长度

--- PHP/PHP数组函数/34array_pad.php
+++ PHP/PHP数组函数/34array_pad.php
@@ -1,18 +1,22 @@
 <?php
 
-$a=['a','d','b','z','e'];
-$b=array_multisort($a);
-var_dump($a);
+$a=array("aa","bb");
+$b=array_pad($a,5,"cc");
+print_r($b);
 
-// array(5) {
-//     [0]=>
-//   string(1) "a"
-//     [1]=>
-//   string(1) "b"
-//     [2]=>
-//   string(1) "d"
-//     [3]=>
-//   string(1) "e"
-//     [4]=>
-//   string(1) "z"
-// }
+// Array
+// (
+// [0] =>; aa
+// [1] =>; bb
+// [2] =>; cc
+// [3] =>; cc
+// [4] =>; cc
+// )
+//
+
+echo "<br>";
+$d=array("aa","bb");
+$e=array_pad($a,-5,"cc");
+print_r($e);
+
+// Array ( [0] => cc [1] => cc [2] => cc [3] => aa [4] => bb )
