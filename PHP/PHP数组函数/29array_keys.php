22ff7659fce4e8d2e6f5ab839f070adac020cf52
7117 <qinglangsalaheiyou@163.com>
2019-09-30 16:40:36 +0800

    array_keys

---
+++ PHP/PHP数组函数/29array_keys.php
@@ -0,0 +1,18 @@
+<?php
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+
+$aa=array_keys($a1);
+
+var_dump($aa);
+
+// array(4) {
+//     [0]=&gt;
+//     string(1) "a"
+//     [1]=&gt;
+//   string(1) "b"
+//     [2]=&gt;
+//   string(1) "c"
+//     [3]=&gt;
+//   string(1) "d"
+// }
