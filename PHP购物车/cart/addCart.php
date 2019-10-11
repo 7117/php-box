268a9c39ad5dd8900ace8b46a4c5be5cd6bcae21
7117 <qinglangsalaheiyou@163.com>
2019-10-12 00:38:14 +0800

    购物车的添加

--- PHP购物车/cart/addCart.php
+++ PHP购物车/cart/addCart.php
@@ -1,2 +1,50 @@
 <?php
+// 1.接受数据
+// 2.处理数据
+// 3.完成添加
+// 4.返回结果
+
+$productid=intval($_POST['productid']);
+$num=intval($_POST['num']);
+
+session_start();
+
+$userid=1;
+
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=test1","root","root",
+        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $pdo->query("set names utf8");
+    $sql="select price from shop_product where id=?";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([$productid]);
+    $data=$stmt->fetch(PDO::FETCH_ASSOC);
+
+    $price=$data['price'];
+    $create=time();
+    $sql="insert into shop_cart (id,productid,userid,num,price,createtime) 
+          values(?,?,?,?,?,?)";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([null,$productid,$userid,$num,$price,$create]);
+    $rows=$stmt->rowCount();
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+if($rows){
+    $response=[
+            "code"=>1,
+            "mes"=>"success",
+            "price"=>$data
+    ];
+    echo json_encode($response);
+}else{
+    $response=[
+        "code"=>0,
+        "mes"=>"fail"
+    ];
+    echo json_encode($response);
+}
+
+
 
--- PHP购物车/cart/js/clickbuy.js
+++ PHP购物车/cart/js/clickbuy.js
@@ -1,10 +1,10 @@
 //ajax请求php脚本完成数据的添加 shop_cart
 function addCart(productid){
-    alert("11");
+    alert("点击购物车");
     var url = "addCart.php";
     var data = {"productid":productid, "num":parseInt($("#number").val())};
     var success= function(response){
-        if(response.errno == 0){
+        if(response.code==1){
             alert('加入购物车成功');
         }else{
             alert('加入购物车失败');
