1314118f7f7a5872aebcefa035e0ace23bb0c726
7117 <qinglangsalaheiyou@163.com>
2019-10-15 10:26:45 +0800

    修改strtotime

--- PHP时间函数/3.timezone.php
+++ PHP时间函数/3.timezone.php
@@ -1,4 +1,9 @@
 <?php
-date_default_timezone_set("Asia/Shanghai");
+header('content-type:text/html;charset=utf-8');
+
+// date_default_timezone_set("Asia/Shanghai");
+ini_set('date.timezone','Asia/Shanghai');
+$zone=date_default_timezone_get();
 echo date("ymd his");
+var_dump($zone);
 
--- PHP时间函数/4.strtotime.php
+++ PHP时间函数/4.strtotime.php
@@ -3,9 +3,18 @@ date_default_timezone_set("Asia/Shanghai");
 
 $startdate=time();
 $enddate = strtotime("+6 weeks",$startdate);
-
-var_dump($enddate);
-
-
 $a=date("YMD h:i:s",$enddate);
 var_dump($a);
+
+$time=date("YMD h:i:s",strtotime("now")+24*3600);
+var_dump($time);
+echo "<br>";
+$time=date("YMD h:i:s",strtotime("+5 days")+24*3600);
+var_dump($time);
+echo "<br>";
+$time=date("YMD h:i:s",strtotime("+1 month")+24*3600);
+var_dump($time);
+echo "<br>";
+$time=date("YMD h:i:s",strtotime("+2 years 1 month 12 days")+24*3600);
+var_dump($time);
+echo "<br>";
