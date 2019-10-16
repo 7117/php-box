a2b4211353f46c04c3181e4c05d3026b3cb3c4cb
7117 <qinglangsalaheiyou@163.com>
2019-10-16 16:01:34 +0800

    添加读取excel的代码与笔记

--- PHPExcel/user/read.php
+++ PHPExcel/user/read.php
@@ -1,5 +1,10 @@
 <?php
-
+//1.进行指定哪一个excel PHPExcel_IOFactory::identify
+//2.进行实例化读取器    PHPExcel_IOFactory::createReader($type)
+//3.指定读取的sheet    $read->setLoadSheetsOnly($sheetname);
+//4.进行加载文件        $excel=$read->load($name);
+//5.获取sheet的个数    $count=$excel->getSheetCount()
+//6.进行获取sheet输出   $excel->getSheet($i)->toArray()
 $dir=dirname(__FILE__);
 include $dir.'.\..\Classes\PHPExcel\IOFactory.php';
 $name=$dir.'.\..\read.xlsx';
@@ -24,6 +29,10 @@ echo "<br>";
 echo "<br>";
 
 // 分步进行获取
+//1.实例化excel
+//2.进行获取全部sheet进行遍历
+//3.进行获取全部row进行遍历
+//4.进行获取每一个表格cell进行遍历
 foreach($excel->getWorksheetIterator() as $sheet){
     foreach($sheet->getRowIterator() as $row){
         if($row->getRowIndex()<2){
