646d5d400c412929b179ef5e8236c04794dab06c
7117 <qinglangsalaheiyou@163.com>
2020-01-19 18:30:04 +0800

    strpos

--- PHP字符串函数/1.strpos.php
+++ PHP字符串函数/1.strpos.php
@@ -16,6 +16,25 @@ $num=strripos('aaavV',"v");
 var_dump($num);
 echo "<br>";
 
+$num=strpos('avV',"av");
+var_dump($num);
+echo "<br>";
+
+$num=(0==FALSE);
+var_dump($num);
+echo "<br>";
+
+$num=strpos('a,b,c', 'a');
+var_dump($num);
+echo "<br>";
+
+if (strpos('a,b,c', 'a')!==false) {
+    echo "1";
+}else{
+    echo "2";
+}
+echo "<br>";
+
 // int(4)
 // int(3)
 // int(3)
