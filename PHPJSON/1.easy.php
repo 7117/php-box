8717fc0bf352f9d9995190913ebca405aeb5f919
7117 <qinglangsalaheiyou@163.com>
2020-03-03 17:04:01 +0800

    json

--- PHPJSON/1.easy.php
+++ PHPJSON/1.easy.php
@@ -1,12 +1,45 @@
 <?php
 
-$a=[
-    "a"=>1,
-    "b"=>2,
-    "c"=>3,
-];
-
-// {"a":"1","b":"2","c":"3"}
+// {"a":1,"b":2,"c":3}
+// a:3:{s:1:"a";i:1;s:1:"b";i:2;s:1:"c";i:3;}
+$a = array(
+    "a" => 1,
+    "b" => 2,
+    "c" => 3,
+);
 echo json_encode($a);
+echo "-----";
+echo serialize($a);
+
+// [{"a":1,"b":2,"c":3}]
+// a:1:{i:0;a:3:{s:1:"a";i:1;s:1:"b";i:2;s:1:"c";i:3;}}
 echo "<br>";
+$a = array(
+    array(
+        "a" => 1,
+        "b" => 2,
+        "c" => 3,
+    )
+);
+
+
+echo json_encode($a);
+echo "-----";
+echo serialize($a);
+
+// [[{"a":1,"b":2,"c":3}]]
+// a:1:{i:0;a:1:{i:0;a:3:{s:1:"a";i:1;s:1:"b";i:2;s:1:"c";i:3;}}}
+echo "<br>";
+$a = array(
+    array(
+        array(
+            "a" => 1,
+            "b" => 2,
+            "c" => 3,
+        )
+    )
+);
+
+echo json_encode($a);
+echo "-----";
 echo serialize($a);
