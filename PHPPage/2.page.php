c147034d2777d49c67c2967af2b0e1d8210a7610
7117 <qinglangsalaheiyou@163.com>
2020-04-21 09:26:56 +0800

    page

--- PHPPage/2.page.php
+++ PHPPage/2.page.php
@@ -1,6 +1,8 @@
+<html>
 <?php
 
-$p = $_GET['p'];
+$p = isset($_GET['p'])?$_GET['p']:1;
+
 $limit = ($p - 1) * 2;
 $offset = 2;
 
@@ -37,11 +39,51 @@ foreach ($info as $k => $v) {
     echo "</tr>";
 }
 
+$self = $_SERVER['PHP_SELF'];
+var_dump($self);
+$prv = $p - 1;
+$aft = $p + 1;
+$page_num=ceil($info2/2);
+
+$pjian2=$p-2;
+$pjia2=$p+2;
+
+if($p==1){
+    $prv=1;
+}
+if($p>=$page_num){
+    $aft=$page_num;
+}
+
+if($p!=1){
+    echo "<a href='{$self}?p=1'>首页</a>";
+}
+echo "<a href='{$self}?p={$prv}'>上一页</a>";
+
+echo "......";
+
+$jian2=$p-1;
+$jia2=$p+1;
+echo "<a href='{$self}?p={$jian2}'>$jian2</a>";
+echo "<a href='{$self}?p={$p}'>$p</a>";
+echo "<a href='{$self}?p={$jia2}'>$jia2</a>";
+
+echo "......";
+
+echo "<a href='{$self}?p={$aft}'>下一页</a>";
+if($p!=$page_num){
+    echo "<a href='{$self}?p={$page_num}'>尾页</a>";
+}
+echo "<br>";
+
 
 echo '</table>';
 
-
-echo "{$info2}";
+echo "总的页数：{$page_num}";
 echo "<br>";
-echo $p;
+echo '当前页数' . $p;
+
+
+?>
+</html>
 
