3c9267019b658238430c662f9ab767889f3fd33b
7117 <qinglangsalaheiyou@163.com>
2020-05-13 21:44:14 +0800

    code

---
+++ PHPDesignPattern/fiv/Db.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PHPDesignPattern\two;
+
+class Db
+{
+    private static $db;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$db) {
+            return self::$db;
+        } else {
+            self::$db = new self();
+            return self::$db;
+        }
+    }
+
+}
---
+++ PHPDesignPattern/for/Db.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PHPDesignPattern\two;
+
+class Db
+{
+    private static $db;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$db) {
+            return self::$db;
+        } else {
+            self::$db = new self();
+            return self::$db;
+        }
+    }
+}
