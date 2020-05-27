bfb5e07f5fbc3d188b58383b674c6454e825822d
7117 <qinglangsalaheiyou@163.com>
2020-05-27 12:28:47 +0800

    preg_match

--- PHP正则函数/018.img.php
+++ PHP正则函数/018.img.php
@@ -1,11 +1,14 @@
 <?php
 
-$reg = '/<img src="([^"].+?)" [^>]* /';
+$reg = '/<img[^>]*src="([^"]*)" (.+)>/us';
+// $reg = '/<img[^>]*src="(.+?)">/us';
 $str = '<img src="https://profile.csdnimg.cn/0/9/7/3_fujian9544" class="avatar_pic" username="fujian9544">';
 preg_match($reg, $str, $con);
 var_dump($con);
 
-// D:\phpstudy\PHPTutorial\WWW\phpCollection\PHP正则函数\18.img.php:6:
-// array (size=2)
-//   0 => string '<img src="https://profile.csdnimg.cn/0/9/7/3_fujian9544" class="avatar_pic" ' (length=76)
+
+// D:\phpstudy\PHPTutorial\WWW\phpCollection\PHP正则函数\018.img.php:7:
+// array (size=3)
+//   0 => string '<img src="https://profile.csdnimg.cn/0/9/7/3_fujian9544" class="avatar_pic" username="fujian9544">' (length=98)
 //   1 => string 'https://profile.csdnimg.cn/0/9/7/3_fujian9544' (length=45)
+//   2 => string 'class="avatar_pic" username="fujian9544"' (length=40)
