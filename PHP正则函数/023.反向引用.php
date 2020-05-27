6e06355d4e8ee05558c501db4e8b1ef7bf010fa8
7117 <qinglangsalaheiyou@163.com>
2020-05-27 10:15:23 +0800

    preg_match

--- PHP正则函数/023.反向引用.php
+++ PHP正则函数/023.反向引用.php
@@ -1,6 +1,12 @@
 <?php
 
-$reg = '/([is].*?)/';
+$reg = '/(.)\1/u';
 $str = 'isswcwcscscscdsis';
 preg_match($reg, $str, $con);
 var_dump($con);
+
+
+// D:\phpstudy\PHPTutorial\WWW\phpCollection\PHP正则函数\023.反向引用.php:6:
+// array (size=2)
+//   0 => string 'ss' (length=2)
+//   1 => string 's' (length=1)
